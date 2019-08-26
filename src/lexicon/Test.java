4da219c9af465f510782f8394f09361a94338e64
RomuloRPS <romulo.rps@hotmail.com>
2019-08-26 19:05:01 -0300

    Alterações test

--- src/lexicon/Test.java
+++ src/lexicon/Test.java
@@ -1,6 +1,9 @@
 package lexicon;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
@@ -13,7 +16,35 @@ public class Test {
 		List<Token> st = lv.getPossibleTokens();
 		
 		List<String> lines = new ArrayList<String>();
-		lines.add("linha 10 20");
+		
+		
+		Path text_path = Paths.get("/tmp", "test.txt");
+		try {
+			List<String> FullText = Files.readAllLines(text_path);
+			
+			Stack<String> stack = new Stack();
+		
+			
+			for (String line : FullText) {
+				
+				stack.push(line);
+					
+			}
+			
+			Stack allLines = new Stack();
+			allLines = reverse(stack);
+			
+			while(!allLines.empty()) {
+				lines.add((String) allLines.pop());
+			}
+		
+			
+		}catch(IOException e) {
+			System.out.println(e);
+		}
+		
+		
+		
 		Stack<Token> stk = lv.validate(lines);
 		System.out.println(stk.size());
 		while(!stk.empty()) {
@@ -28,6 +59,14 @@ public class Test {
 		}
 		
 		
+	}
+	
+	public static Stack reverse(Stack stack){
+		Stack newStack = new Stack();
+		while(!stack.empty()) {
+			newStack.push( stack.pop());
+		}
+		return newStack;
 	}
 
 }
